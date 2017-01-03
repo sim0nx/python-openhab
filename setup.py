7027611ae9092b85d041b66cdf39bad3d30609e6
Georges Toth <georges@trypill.org>
2017-01-04 00:54:29 +0100

    fix readme path

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ VERSION = (2, 0)
 __version__ = VERSION
 __versionstr__ = '.'.join(map(str, VERSION))
 
-f = open(join(dirname(__file__), 'README.md'))
+f = open(join(dirname(__file__), 'README.rst'))
 long_description = f.read().strip()
 f.close()
 
