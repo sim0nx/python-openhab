30f9869a8b3dcb251c3cd4917e1e5ef6655b898e
Georges Toth <georges@trypill.org>
2021-01-06 00:41:26 +0100

    raise on invalid type

--- openhab/items.py
+++ openhab/items.py
@@ -144,10 +144,14 @@ class ItemFactory:
       function_params (List of str): optional list of function Params. no documentation found
     """
     paramdict: typing.Dict[str, typing.Union[str, typing.List[str], typing.Dict[str, typing.Union[str, typing.List]]]] = {}
-    itemtypename = _type
+
     if isinstance(_type, type):
       if issubclass(_type, Item):
         itemtypename = _type.TYPENAME
+      else:
+        raise ValueError(f'_type parameter must be a valid subclass of type *Item* or a string name of such a class; given value is "{str(_type)}"')
+    else:
+      itemtypename = _type
 
     if quantity_type is None:
       paramdict['type'] = itemtypename
